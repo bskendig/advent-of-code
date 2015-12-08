57c9264fccfb821faa2cf8d9966f61820df90c72
Brian Kendig <brian.kendig@disney.com>
2015-12-08 17:20:17 -0500

    WIP

--- 8/8/main.swift
+++ 8/8/main.swift
@@ -10,8 +10,32 @@ import Foundation
 
 func getInput() -> String {
     let bundle = NSBundle.mainBundle()
-    let path = bundle.pathForResource("input", ofType: "txt")
+    let path = bundle.pathForResource("simple", ofType: "txt")
     return try! NSString(contentsOfFile: path!, encoding: NSUTF8StringEncoding) as String
+}
+
+func replaceMultipleStrings(s: String, pattern: String, replace: (String) -> String) -> String {
+    let regex = try! NSRegularExpression(pattern: pattern, options: [])
+    var offset = 0 // keep track of range changes in the string due to replacements
+    var sc = s  // copy the string so it's mutable
+
+    for result in regex.matchesInString(sc, options: [], range: NSMakeRange(0, sc.characters.count)) {
+        var resultRange = result.range
+        resultRange.location += offset  // update it based on the offset
+
+        let match = regex.replacementStringForResult(result, inString: sc, offset: offset, template: "$0")
+        let replacement = replace(match)
+
+        // make the replacement
+        sc.replaceRange(
+            sc.startIndex.advancedBy(resultRange.location)
+                ..< sc.startIndex.advancedBy(resultRange.location + resultRange.length),
+            with: replacement)
+
+        // update the offset based on the replacement
+        offset += (replacement.characters.count - resultRange.length)
+    }
+    return sc
 }
 
 func main() {
@@ -19,9 +43,39 @@ func main() {
 
     // the regexps have to be escaped twice for some reason
     // the pattern is: \[\"]|\x..
-    let escape = try! NSRegularExpression(pattern: "\\\\[\\\\\\\"]|\\\\x..", options: [])
+
+    // part 1
 
     var count = 0
+    for name in names {
+        if name.isEmpty { continue }
+
+        var pattern = "\\\\[\\\\\\\"]|\\\\x"
+        var replace = {
+            (match: String) -> String in
+            if match == "\\\"" { return "\\\\\\\"" }
+            else if match == "\\\\" { return "\\\\\\\\" }
+            else { return "\\" + match }
+        }
+        var s = replaceMultipleStrings(name, pattern: pattern, replace: replace)
+
+        pattern = "^\"|\"$"
+        replace = {
+            (match: String) -> String in
+            return ""
+        }
+        s = replaceMultipleStrings(s, pattern: pattern, replace: replace)
+
+        let c = s.characters.count - name.characters.count
+        count += c
+
+        print("\(c) \(name) -> \(s)")
+    }
+
+    // part 2
+
+    let escape = try! NSRegularExpression(pattern: "\\\\[\\\\\\\"]|\\\\x..", options: [])
+
     for name in names {
         if name.isEmpty { continue }
         let range = name.startIndex.advancedBy(1) ..< name.endIndex.advancedBy(-1)
@@ -30,41 +84,29 @@ func main() {
             range: NSMakeRange(0, shortenedName1.characters.count), withTemplate: "*")
         let c = name.characters.count - shortenedName2.characters.count
 
-//        print("\(c) \(name) -> \(shortenedName2)")
-        //        print(name + " -> " + NSRegularExpression.escapedPatternForString(name))
         count += c
     }
-    print(count)
+    print("\(count) characters in the first part")
 
     count = 0
     for name in names {
         if name.isEmpty { continue }
-        var n = name
-        var offset = 0 // keep track of range changes in the string due to replacements
-        for result in escape.matchesInString(name, options: [], range: NSMakeRange(0, name.characters.count)) {
-            var resultRange = result.range
-            resultRange.location += offset  // update it based on the offset
 
-            let match = escape.replacementStringForResult(result, inString: n, offset: offset, template: "$0")
-            var replacement = "*"
-            if match == "\\\"" { replacement = "\\\\\\\"" }
-            else if match == "\\\\" { replacement = "\\\\\\\\" }
-            else { replacement = "\\" + match }
-
-            // make the replacement
-            n.replaceRange(
-                n.startIndex.advancedBy(resultRange.location)
-                    ..< n.startIndex.advancedBy(resultRange.location + resultRange.length),
-                with: replacement)
-
-            // update the offset based on the replacement
-            offset += (replacement.characters.count - resultRange.length)
+        let replace = {
+            (match: String) -> String in
+            if match == "\\\"" { return "\\\\\\\"" }
+            else if match == "\\\\" { return "\\\\\\\\" }
+            else { return "\\" + match }
         }
-        let c = n.characters.count - name.characters.count + 4
-        print("\(c) \(name) -> \(n)")
+        let pattern = "\\\\[\\\\\\\"]|\\\\x"
+        var s = replaceMultipleStrings(name, pattern: pattern, replace: replace)
+//        s = replaceMultipleStrings(s, pattern: "^\"|\"$", replacements: ["\"": "\\\""])
+        s = "\"\(s)\""
+
+        let c = s.characters.count - name.characters.count
         count += c
     }
-    print(count)
+    print("\(count) characters in the second part")
 }
 
 main()
