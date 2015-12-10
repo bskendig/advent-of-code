c37afb8c59d5864343c537bd11c70a3f36b245a6
Brian Kendig <brian.kendig@disney.com>
2015-12-10 16:50:37 -0500

    10: Attempt at optimization

--- 10/10/main.swift
+++ 10/10/main.swift
@@ -8,33 +8,39 @@
 
 import Foundation
 
-func iterate(s: String) -> String {
-    var new = ""
+func iterate(inout s: String) -> String {
+    var new: [String] = []
     var prev: Character = " "
     var count = 0
     for c in s.characters {
         if c == prev {
             count++
         } else {
-            if count != 0 { new += "\(count)\(prev)" }  // starting condition
+            if count != 0 { new.append("\(count)\(prev)") }  // starting condition
             prev = c
             count = 1
         }
     }
-    new += "\(count)\(prev)"
-    return new
+    new.append("\(count)\(prev)")
+    return new.joinWithSeparator("")
 }
 
 func main() {
     var input = "1113222113"
     for _ in 1...40 {
-        input = iterate(input)
+        input = iterate(&input)
     }
     print("After 40 iterations, we have \(input.characters.count) characters")
     for _ in 1...10 {
-        input = iterate(input)
+        input = iterate(&input)
     }
     print("After 50 iterations, we have \(input.characters.count) characters")
+
+//    for i in 1...100 {
+//        input = iterate(&input)
+//        print(i)
+//    }
+
 }
 
 main()
