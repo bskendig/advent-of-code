33f4bfa7b2df3c5ea9f029bffdd075241e491959
Brian Kendig <brian.kendig@disney.com>
2015-12-14 16:27:19 -0500

    14 cleanup

--- 14/14/main.swift
+++ 14/14/main.swift
@@ -9,10 +9,10 @@
 import Foundation
 
 struct Reindeer {
+    var name: String
     var speed: Int
     var flying: Int
     var resting: Int
-    var points: Int
 }
 
 func getInput() -> String {
@@ -29,17 +29,25 @@ func getMatches(s: String, matches: [NSTextCheckingResult]) -> [String] {
     return a
 }
 
-func distance(reindeer: Reindeer, time: Int) -> Int {
-    return (time / (reindeer.flying + reindeer.resting)) * (reindeer.speed * reindeer.flying)
-        + min(time % (reindeer.flying + reindeer.resting), reindeer.flying) * reindeer.speed
+func distance(reindeer: Reindeer, atTime: Int) -> Int {
+    return (atTime / (reindeer.flying + reindeer.resting)) * (reindeer.speed * reindeer.flying)
+        + min(atTime % (reindeer.flying + reindeer.resting), reindeer.flying) * reindeer.speed
+}
+
+func getDistances(reindeer: [Reindeer], atTime: Int) -> [Int:[String]] {
+    var distances: [Int:[String]] = [:]
+    for reindeer in reindeer {
+        let d = distance(reindeer, atTime: atTime)
+        if distances[d] == nil { distances[d] = [] }
+        distances[d]!.append(reindeer.name)
+    }
+    return distances
 }
 
 func main() {
     let reindeerLines = getInput().componentsSeparatedByString("\n")
+    var allDeer: [Reindeer] = []
     let raceLength = 2503
-    var winningName = ""
-    var winningDistance = 0
-    var allDeer: [String:Reindeer] = [:]
 
     // Vixen can fly 19 km/s for 7 seconds, but then must rest for 124 seconds.
     let regex = try! NSRegularExpression(
@@ -50,32 +58,31 @@ func main() {
         if !matches.isEmpty {
             // Vixen, 19, 7, 124
             let a = getMatches(line, matches: matches)
-            let name = a[0], reindeer = Reindeer(speed: Int(a[1])!, flying: Int(a[2])!, resting: Int(a[3])!, points: 0)
-            allDeer[name] = reindeer
-            let d = distance(reindeer, time: raceLength)
-            if (d > winningDistance) {
-                winningDistance = d
-                winningName = name
-            }
+            let reindeer = Reindeer(name: a[0], speed: Int(a[1])!, flying: Int(a[2])!, resting: Int(a[3])!)
+            allDeer.append(reindeer)
         }
     }
-    print("\(winningName): \(winningDistance)")
 
+    var distances = getDistances(allDeer, atTime: raceLength)
+    let (winningDistance, winningDeer1) = distances.maxElement({ (a, b) -> Bool in
+        return a.0 < b.0
+    })!
+    print("First race winner: \(winningDeer1.joinWithSeparator(" and ")) at \(winningDistance) km.")
+
+    var points: [String:Int] = [:]
+    for reindeer in allDeer {
+        points[reindeer.name] = 0
+    }
     for t in 1 ... raceLength {
-        var distances: [Int:[String]] = [:]
-        for (name, reindeer) in allDeer {
-            let d = distance(reindeer, time: t)
-            if distances[d] == nil { distances[d] = [] }
-            distances[d]!.append(name)
-        }
+        distances = getDistances(allDeer, atTime: t)
         let bestDistance = distances.keys.maxElement()
         for name in distances[bestDistance!]! {
-            allDeer[name]!.points++
+            points[name]!++
         }
     }
-//    winningName = allDeer.maxElement({$0.1.points < $1.1.points})
-    print(winningName)
-    print(allDeer)
+    let winningScore = points.values.maxElement()!
+    let winningDeer2 = points.filter({ $0.1 == winningScore }).map({ $0.0 })
+    print("Second race winner: \(winningDeer2.joinWithSeparator(" and ")) with \(winningScore) points.")
 }
 
 main()
