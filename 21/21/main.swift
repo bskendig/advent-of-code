9cb99deb073c5cbe62518cbe8f8e39a608358a81
Brian Kendig <brian@mac.com>
2015-12-21 17:35:31 -0500

    21 done

--- 21/21/main.swift
+++ 21/21/main.swift
@@ -8,15 +8,28 @@
 
 import Foundation
 
+func doIWin(myDamage: Int, myArmor: Int) -> Bool {
+    let bossHP = 109, bossDamage = 8, bossArmor = 2
+    let myHP = 100
+
+    let damageToMeEachTurn = max(bossDamage - myArmor, 1)
+    let damageToBossEachTurn = max(myDamage - bossArmor, 1)
+
+    let turnsToKillMe = ceil(Float(myHP / damageToMeEachTurn))
+    let turnsToKillBoss = ceil(Float(bossHP / damageToBossEachTurn))
+
+    return turnsToKillBoss <= turnsToKillMe
+}
+
 func main() {
-    let weapons: [String:(Int, Int, Int)] = [
+    let weapons: [String:(cost: Int, damage: Int, armor: Int)] = [
         "Dagger":     (  8, 4, 0),
         "Shortsword": ( 10, 5, 0),
         "Warhammer":  ( 25, 6, 0),
         "Longsword":  ( 40, 7, 0),
         "Greataxe":   ( 74, 8, 0)
     ]
-    let armor: [String:(Int, Int, Int)] = [
+    let armors: [String:(cost: Int, damage: Int, armor: Int)] = [
         "none":       (  0, 0, 0),
         "Leather":    ( 13, 0, 1),
         "Chainmail":  ( 31, 0, 2),
@@ -24,9 +37,9 @@ func main() {
         "Bandedmail": ( 75, 0, 4),
         "Platemail":  (102, 0, 5)
     ]
-    let rings: [String:(Int, Int, Int)] = [
-        "none":       (  0, 0, 0),
-        "none":       (  0, 0, 0),
+    let rings: [String:(cost: Int, damage: Int, armor: Int)] = [
+        "none1":      (  0, 0, 0),
+        "none2":      (  0, 0, 0),
         "Damage +1":  ( 25, 1, 0),
         "Damage +2":  ( 50, 2, 0),
         "Damage +3":  (100, 3, 0),
@@ -35,6 +48,28 @@ func main() {
         "Defense +3": ( 80, 0, 3)
     ]
 
+    var success: [(String, Int)] = []
+    var failure: [(String, Int)] = []
+
+    for weapon in weapons {
+        for armor in armors {
+            for ring1 in rings {
+                for ring2 in rings where ring1.0 != ring2.0 {
+                    let equipment = [weapon, armor, ring1, ring2].map({ $0.0 }).joinWithSeparator(", ")
+                    let cost = [weapon, armor, ring1, ring2].map({ $0.1.cost }).reduce(0, combine: +)
+                    let damage = [weapon, armor, ring1, ring2].map({ $0.1.damage }).reduce(0, combine: +)
+                    let protection = [weapon, armor, ring1, ring2].map({ $0.1.armor }).reduce(0, combine: +)
+                    if doIWin(damage, myArmor: protection) {
+                        success.append((equipment, cost))
+                    } else {
+                        failure.append((equipment, cost))
+                    }
+                }
+            }
+        }
+    }
+    print("Minimum equipment to win the fight is \(success.minElement({ $0.1 < $1.1 })!).")
+    print("Maximum equipment to lose the fight is \(failure.maxElement({ $0.1 < $1.1 })!).")
 }
 
 main()
