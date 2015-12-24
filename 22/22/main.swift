841cd6d5ae497b72443e90bc44c1bda69ef829e4
Brian Kendig <brian@mac.com>
2015-12-23 21:27:00 -0500

    22 done

--- 22/22/main.swift
+++ 22/22/main.swift
@@ -10,11 +10,6 @@ import Foundation
 
 enum Spell: Int {
     case MagicMissile, Drain, Shield, Poison, Recharge
-//    static let count: Int = {
-//        var max: Int = 0
-//        while let _ = Spell(rawValue: max) { max += 1 }
-//        return max
-//    }()
 }
 
 let spellCost: [Spell:Int] = [
@@ -31,6 +26,7 @@ struct GameState {
     var myMana: Int
     var bossHP: Int
     var bossDamage: Int
+    var hardMode: Bool
 }
 
 func applyEffects(inout gameState: GameState, inout myArmor: Int) {
@@ -38,13 +34,13 @@ func applyEffects(inout gameState: GameState, inout myArmor: Int) {
         switch spell {
         case .Shield:
             myArmor = 7
-            print("Shield sets armor to 7.")
+//            print("Shield sets armor to 7.")
         case .Poison:
             gameState.bossHP -= 3
-            print("Poison deals 3 damage.")
+//            print("Poison deals 3 damage.")
         case .Recharge:
             gameState.myMana += 101
-            print("Recharge provides 101 mana.")
+//            print("Recharge provides 101 mana.")
         default:
             break
         }
@@ -57,118 +53,109 @@ func applyEffects(inout gameState: GameState, inout myArmor: Int) {
 }
 
 func oneRound(casting: Spell, var gameState: GameState) -> GameState {
-
-    // player turn
-    print("-- Player turn --")
-    print("Player has \(gameState.myHP) hit points, \(gameState.myMana) mana")
-    print("Boss has \(gameState.bossHP) hit points")
+//    print("-- Player turn --")
+//    print("Player has \(gameState.myHP) hit points, \(gameState.myMana) mana")
+//    print("Boss has \(gameState.bossHP) hit points")
     var myArmor = 0
+
+    if gameState.hardMode {
+        gameState.myHP--
+        if gameState.myHP <= 0 {
+//            print("Player is dead.")
+            return gameState
+        }
+    }
+
     applyEffects(&gameState, myArmor: &myArmor)
     if gameState.bossHP <= 0 {
-        print("Boss is dead.")
+//        print("Boss is dead.")
         return gameState
     }
     gameState.myMana -= spellCost[casting]!
     switch casting {
     case .MagicMissile:
         gameState.bossHP -= 4
-        print("Player casts Magic Missile, dealing 4 damage.")
+//        print("Player casts Magic Missile, dealing 4 damage.")
     case .Drain:
         gameState.bossHP -= 2
         gameState.myHP += 2
-        print("Player casts Drain, dealing 2 damage, and healing 2 hit points.")
+//        print("Player casts Drain, dealing 2 damage, and healing 2 hit points.")
     case .Shield:
         gameState.spellDurationsRemaining[.Shield] = 6
-        print("Player casts Shield, increasing armor by 7.")
+//        print("Player casts Shield, increasing armor by 7.")
     case .Poison:
         gameState.spellDurationsRemaining[.Poison] = 6
-        print("Player casts Poison.")
+//        print("Player casts Poison.")
     case .Recharge:
         gameState.spellDurationsRemaining[.Recharge] = 5
-        print("Player casts Recharge.")
+//        print("Player casts Recharge.")
     }
     if gameState.bossHP <= 0 {
-        print("Boss is dead.")
+//        print("Boss is dead.")
         return gameState
     }
-    print("")
+//    print("")
 
-    print("-- Boss turn --")
-    print("Player has \(gameState.myHP) hit points, \(gameState.myMana) mana")
-    print("Boss has \(gameState.bossHP) hit points")
+//    print("-- Boss turn --")
+//    print("Player has \(gameState.myHP) hit points, \(gameState.myMana) mana")
+//    print("Boss has \(gameState.bossHP) hit points")
     applyEffects(&gameState, myArmor: &myArmor)
     if gameState.bossHP <= 0 {
-        print("Boss is dead.")
+//        print("Boss is dead.")
         return gameState
     }
     let damage = gameState.bossDamage - myArmor
     gameState.myHP -= damage
-    print("Boss attacks for \(damage) damage!")
+//    print("Boss attacks for \(damage) damage!")
     if gameState.myHP <= 0 {
-        print("Player is dead.")
+//        print("Player is dead.")
         return gameState
     }
-    print("")
+//    print("")
 
     return gameState
 }
 
-func lowestManaFromThisState(gameState: GameState) -> Int {
-//    if gameState.bossHP <= 0 {
-//
-//    } else if gameState.myHP <= 0 {
-//
-//    }
+func lowestManaFromThisState(gameState: GameState, var notToExceed: Int) -> Int {
     var manaUsage: [Spell:Int] = [:]
-    for spell: Spell in [.MagicMissile, .Drain, .Shield, .Poison, .Recharge] {
-        if spellCost[spell] > gameState.myMana {
+    for spell: Spell in [.MagicMissile, .Drain, .Shield, .Poison, .Recharge].filter({ !(gameState.spellDurationsRemaining[$0] > 1)  }) {
+        if spellCost[spell] > gameState.myMana || spellCost[spell] > notToExceed {
             continue
         }
         let newGameState = oneRound(spell, gameState: gameState)
         if newGameState.bossHP <= 0 {
             manaUsage[spell] = spellCost[spell]
         } else if newGameState.myHP > 0 {
-            manaUsage[spell] = spellCost[spell]! + lowestManaFromThisState(newGameState)
+            let lowestMana = lowestManaFromThisState(newGameState, notToExceed: notToExceed - spellCost[spell]!)
+            if lowestMana < notToExceed {
+                notToExceed = lowestMana
+            }
+            manaUsage[spell] = spellCost[spell]! + lowestMana
         }
     }
     if manaUsage.count == 0 {
         return 99999
     }
     let bestSpellToUse = manaUsage.minElement({ $0.1 < $1.1 })
-    // bestSpellToUse.0 is the spell
+    // bestSpellToUse.0 is the spell, in case I want to print those out
     return bestSpellToUse!.1
 }
 
 func main() {
-//    let bossStartingHP = 58
-//    let bossDamage = 9
-//    let myStartingHP = 50
-//    let myStartingMana = 500
+    let bossStartingHP = 58
+    let bossDamage = 9
+    let myStartingHP = 50
+    let myStartingMana = 500
 
-    let bossStartingHP = 14
-    let bossDamage = 8
-    let myStartingHP = 10
-    let myStartingMana = 250
+    let gameState = GameState(
+        spellDurationsRemaining: [:], myHP: myStartingHP, myMana: myStartingMana, bossHP: bossStartingHP, bossDamage: bossDamage, hardMode: false
+    )
+    print("Least amount of mana to spend to win: \(lowestManaFromThisState(gameState, notToExceed: Int.max))")
 
-    // we have a starting point: my hp and mana, boss hp, spell durations
-    // for each possible spell we can cast from here:
-    //   get a list of all possible outcomes
-
-    var gameState = GameState(spellDurationsRemaining: [:], myHP: myStartingHP, myMana: myStartingMana, bossHP: bossStartingHP, bossDamage: bossDamage)
-
-    print(lowestManaFromThisState(gameState))
-
-
-//    for spell in [Spell.Recharge, Spell.Shield, Spell.Drain, Spell.Poison, Spell.MagicMissile] {
-//        gameState = oneRound(spell, gameState: gameState)
-//        print(gameState)
-//    }
-
-    for _ in 1 ... 500 {
-
-    }
-
-
+    let gameStateHardMode = GameState(
+        spellDurationsRemaining: [:], myHP: myStartingHP, myMana: myStartingMana, bossHP: bossStartingHP, bossDamage: bossDamage, hardMode: true
+    )
+    print("Least amount of mana to spend to win in hard mode: \(lowestManaFromThisState(gameStateHardMode, notToExceed: Int.max))")
 }
 
 main()
