5bbd2ed2f51ed2d73ba5c8ff508d8d6c39e80a7b
Brian Kendig <brian@mac.com>
2015-12-24 19:12:27 -0500

    24 WIP

--- 24/24/main.swift
+++ 24/24/main.swift
@@ -8,14 +8,41 @@
 
 import Foundation
 
+struct WeightCollection {
+    var used: [Int]
+    var leftover: [Int]
+}
+
 func getInput() -> String {
     let bundle = NSBundle.mainBundle()
     let path = bundle.pathForResource("input", ofType: "txt")
     return try! NSString(contentsOfFile: path!, encoding: NSUTF8StringEncoding) as String
 }
 
-func weightsToTotal(total: Int, weights: [Int]) {
+func weightsToTotal(total: Int, weights: [Int]) -> [WeightCollection] {
+    var workingCombinations: [WeightCollection] = []
+    for i in 0 ..< weights.count {
+        var weightsWithOneRemoved = weights
+        let weight: Int = weightsWithOneRemoved.removeAtIndex(i)
+        if weight > total {
+            continue
+        } else if weight == total {
+            let weightCollection = WeightCollection(used: [weight], leftover: weightsWithOneRemoved)
+            workingCombinations.append(weightCollection)
+        } else {
+            let weightCollections = weightsToTotal(total - weight, weights: weightsWithOneRemoved)
+            if !weightCollections.isEmpty {
+                let newWeightCollections = weightCollections.map({ (weightCollection: WeightCollection) -> WeightCollection in
+                    var newWeightCollection = weightCollection
+                    var usedGroup = newWeightCollection.used
+                    return usedGroup.app append(weight)
+                })
+            }
 
+        }
+
+    }
+    return workingCombinations
 }
 
 func main() {
